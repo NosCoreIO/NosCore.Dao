f75e73f8c42e539a10677d7048c97977635c83c5
erwan-joly <erwan.joly@pushpay.com>
2020-04-06 00:18:48 +1200

    start FindAllComposite

--- src/NosCore.Dao/Extensions/DbContextFindAllExtensions.cs
+++ src/NosCore.Dao/Extensions/DbContextFindAllExtensions.cs
@@ -20,23 +20,27 @@ namespace NosCore.Dao.Extensions
         where T : class
         {
             var list = keyValues.ToList();
+            return keyProperty.Length == 1 ? dbSet.FindAll(keyProperty, list) : dbSet.FindAllComposite(keyProperty, list);
+        }
+
+        private static IQueryable<T> FindAll<T, TKey>(this DbSet<T> dbSet, PropertyInfo[] keyProperty, List<TKey> list)
+            where T : class
+        {
             var parameter = Expression.Parameter(typeof(T), "e");
             var methodInfo = typeof(List<TKey>).GetMethod("Contains") ?? throw new InvalidOperationException();
-            Expression expressionToInject = Expression.Empty();
-            var i = 0;
-            foreach (var expression in keyProperty.Select(
-                composite => Expression.MakeMemberAccess(parameter, composite)))
-            {
-                if (i == 0)
-                {
-                    expressionToInject = expression;
-                }
-                else
-                {
-                    expressionToInject = Expression.AndAlso(expressionToInject, expression);
-                }
-            }
-            var body = Expression.Call(Expression.Constant(list, typeof(List<TKey>)), methodInfo, expressionToInject);
+            var body = Expression.Call(Expression.Constant(list, typeof(List<TKey>)), methodInfo,
+                Expression.MakeMemberAccess(parameter, keyProperty.First()));
+            var predicateExpression = Expression.Lambda<Func<T, bool>>(body, parameter);
+            return dbSet.Where(predicateExpression);
+        }
+
+        private static IQueryable<T> FindAllComposite<T, TKey>(this DbSet<T> dbSet, PropertyInfo[] keyProperty, List<TKey> list)
+            where T : class
+        {
+            var parameter = Expression.Parameter(typeof(T), "e");
+            var methodInfo = typeof(List<TKey>).GetMethod("Contains") ?? throw new InvalidOperationException();
+            var body = Expression.Call(Expression.Constant(list, typeof(List<TKey>)), methodInfo,
+                Expression.MakeMemberAccess(parameter, keyProperty.First()));
             var predicateExpression = Expression.Lambda<Func<T, bool>>(body, parameter);
             return dbSet.Where(predicateExpression);
         }
